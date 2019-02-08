42faed99455e772c77cf90502d9c6e9619ef00cc
Noaman Tata <noaman.t@hotmail.fr>
2019-02-08 17:15:14 +0100

    Creation branch

--- app/src/main/java/com/example/modulereco/MainActivity.java
+++ app/src/main/java/com/example/modulereco/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //Button btAnalyse = findViewById(R.id.btAnalyse);
+        //View type Button btAnalyse = findViewById(R.id.btAnalyse);
 
         //btAnalyse.setOnClickListener();
     }
