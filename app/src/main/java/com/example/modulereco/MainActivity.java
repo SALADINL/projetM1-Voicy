35eb2fecdefcafd58ad58b02c8e6809539689adf
Noaman Tata <noaman.t@hotmail.fr>
2019-02-09 17:43:21 +0100

    Début config Phoneme reco

--- app/src/main/java/com/example/modulereco/MainActivity.java
+++ app/src/main/java/com/example/modulereco/MainActivity.java
@@ -5,16 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
 
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+
+import static edu.cmu.pocketsphinx.SpeechRecognizerSetup.defaultSetup;
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
+
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
+    private SpeechRecognizer recognizer;
+    private Button btAnalyse;
+    private HashMap<String, Integer> captions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        captions = new HashMap<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        Button btAnalyse = findViewById(R.id.btAnalyse);
+
+        //runRecognizerSetup();
+
+        btAnalyse = findViewById(R.id.btAnalyse);
 
         btAnalyse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +43,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void alignement() {
+        //recognizer = defaultSetup();
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    @Override
+    public void onTimeout() {
+
     }
 }
