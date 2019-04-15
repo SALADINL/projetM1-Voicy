51d7525c8cd5a5254f78c9d05f998706aa4ab792
Ken <ken.bres@hotmail.fr>
2019-04-16 00:29:33 +0200

    Ajout d'un bouton de lecture du fichier audio.

--- app/src/main/java/com/example/modulereco/Resultat.java
+++ app/src/main/java/com/example/modulereco/Resultat.java
@@ -5,12 +5,8 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.constraint.ConstraintLayout;
-import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -21,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.Switch;
@@ -37,18 +34,17 @@ import java.util.Collections;
 
 public class Resultat  extends Activity
 {
-
 	private Context ctx;
 	private ConstraintLayout rLayout;
 	private ListView listMot = null;
 	private ArrayList<String> listItems = new ArrayList<>();
 	private ArrayAdapter<String> adapter;
 	private PopupWindow popUp;
-	private String filepath;
+	private String filepath, fileType ="";
 	private Bundle bund;
-	private String fileType ="";
 	private TextView titrePopUp = null;
 	private Switch swDap;
+	private ImageView play;
 	private MediaPlayer mediaPlayer;
 
 	@Override
@@ -65,8 +61,8 @@ public class Resultat  extends Activity
 
 		listMot = findViewById(R.id.listMot);
 		adapter = new ArrayAdapter<>(this,
-		android.R.layout.simple_list_item_1,
-		listItems);
+					android.R.layout.simple_list_item_1,
+					listItems);
 		listMot.setAdapter(adapter);
 
 		final File extStorageDir = new File(filepath);
@@ -81,8 +77,6 @@ public class Resultat  extends Activity
 
 		listMot.setOnItemClickListener(new AdapterView.OnItemClickListener()
 		{
-
-
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
 			{
@@ -91,34 +85,42 @@ public class Resultat  extends Activity
 				LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(LAYOUT_INFLATER_SERVICE);
 				View customView = inflater.inflate(R.layout.popup_layout,null);
 
+				popUp = new PopupWindow(customView,
+							ViewGroup.LayoutParams.WRAP_CONTENT,
+							ViewGroup.LayoutParams.WRAP_CONTENT,
+							true);
 
-				popUp = new PopupWindow(
-				customView,
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT,true);
-				//popUp.setWidth((int) (LinearLayout.LayoutParams.MATCH_PARENT*0.8));
 				popUp.setWindowLayoutMode(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-				// Set an elevation value for popup window
-				// Call requires API level 21
-				if(Build.VERSION.SDK_INT >= 21)
-					popUp.setElevation(5.0f);
+				popUp.setElevation(5.0f);
 
-				swDap = customView.findViewById(R.id.swDap); // switch pour le dap
-				ImageButton closeButton = customView.findViewById(R.id.ib_close); // bt close
+				play = customView.findViewById(R.id.btnPlay);
+				play.setOnClickListener(new View.OnClickListener()
+				{
+					@Override
+					public void onClick(View v)
+					{
+						if (!mediaPlayer.isPlaying())
+							mediaPlayer.start();
+					}
+				});
+
+				swDap = customView.findViewById(R.id.swDap);
+				ImageButton closeButton = customView.findViewById(R.id.ib_close);
 				closeButton.setOnClickListener(new View.OnClickListener()
 				{
 					@Override
-					public void onClick(View view)
+					public void onClick(View view) // Dismiss the popup window
 					{
-						// Dismiss the popup window
 						popUp.dismiss();
 						mediaPlayer.stop();
 					}
 				});
 
-				popUp.setOnDismissListener(new PopupWindow.OnDismissListener() {
+				popUp.setOnDismissListener(new PopupWindow.OnDismissListener()
+				{
 					@Override
-					public void onDismiss() {
+					public void onDismiss()
+					{
 						mediaPlayer.stop();
 					}
 				});
@@ -126,11 +128,9 @@ public class Resultat  extends Activity
 				popUp.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
 				popUp.showAtLocation(rLayout, Gravity.CENTER,0,0);
 				popUp.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-
-
-
 				titrePopUp = popUp.getContentView().findViewById(R.id.titrePopUp);
-				if(fileType.equals("-score.txt"))
+
+				if (fileType.equals("-score.txt"))
 					titrePopUp.setText("Phrase n°"+parent.getItemAtPosition(position));
 				else
 					titrePopUp.setText(parent.getItemAtPosition(position)+"");
@@ -138,19 +138,23 @@ public class Resultat  extends Activity
 				filepath = bund.getString("path");
 				filepath += "/" + parent.getItemAtPosition(position);
 
-
 				final ArrayList<String> dataPhone = getData(parent, position, true);
+
 				for (String s : dataPhone)
 					System.out.println("data -> "+dataPhone);
+
 				initRes(dataPhone, true);
-				lecture(parent, position); // lecture du wav
-				if(!fileType.equals("-score.txt")) // si c'est le mode phrase pas de DAP
+				chargerWav(parent, position);
+
+				if (!fileType.equals("-score.txt")) // si c'est le mode phrase pas de DAP
 				{
 					final ArrayList<String> dataDap = getData(parent, position, false);
 					swDap.setVisibility(View.VISIBLE);
-					swDap.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+					swDap.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+					{
 						@Override
-						public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+						public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+						{
 							if (isChecked)
 								initRes(dataDap, false);
 							else
@@ -160,42 +164,52 @@ public class Resultat  extends Activity
 				}
 			}
 		});
-
-
 	}
 
-	private ArrayList<String> getData(AdapterView<?> parent, int position, boolean type) { // type true = phoneme false = DAP
-		String line;
+	private ArrayList<String> getData(AdapterView<?> parent, int position, boolean type) // type true = phoneme false = DAP
+	{
+		String line, typePath = fileType;
 		File file;
 		ArrayList<String> data = new ArrayList<>();
-		String typePath = fileType;
-		if(!type)
+
+		if (!type)
 			typePath = "-score-dap.txt";
-		try {
+
+		try
+		{
 			file = new File(filepath, "/" + parent.getItemAtPosition(position) + typePath);
 			BufferedReader br = new BufferedReader(new FileReader(file));
+
 			while ((line = br.readLine()) != null)
 				data.add(line);
+
 			br.close();
-		} catch (IOException e)
+		}
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
+
 		return data;
 	}
 
 	private void getType()
 	{
 		//Savoir de quel type est le fichier phoneme mots phrase etc
-		if(!listItems.isEmpty())
+		if (!listItems.isEmpty())
 		{
 			File f = new File(filepath+"/"+listItems.get(0));
 			String [] r = f.list();
 			String res;
-			for (String str : r) {
-				if (str.contains("-")) {
+
+			for (String str : r)
+			{
+				if (str.contains("-"))
+				{
 					res = str.substring(str.indexOf("-"));
-					if (res.equals("-score.txt") || res.equals("-score-phoneme.txt")) {
+
+					if (res.equals("-score.txt") || res.equals("-score-phoneme.txt"))
+					{
 						fileType = res;
 						return;
 					}
@@ -204,18 +218,20 @@ public class Resultat  extends Activity
 		}
 	}
 
-	private void lecture(AdapterView<?> parent, int position)
+	private void chargerWav(AdapterView<?> parent, int position)
 	{
 		mediaPlayer = new MediaPlayer();
 		String path = filepath+ "/" + parent.getItemAtPosition(position) + ".wav";
+
 		try
 		{
 			mediaPlayer.setDataSource(path);
 			mediaPlayer.prepare();
-		} catch (Exception e ) {
+		}
+		catch (Exception e )
+		{
 			e.printStackTrace();
 		}
-		mediaPlayer.start();
 	}
 
 	private void initRes(ArrayList<String> output, Boolean phoneSearch)
@@ -247,7 +263,7 @@ public class Resultat  extends Activity
 					break;
 
 				case 1 :
-					col.setText(" Temps (frame) ");
+					col.setText(" Durée (frames) ");
 					break;
 
 				case 2 :
@@ -261,19 +277,20 @@ public class Resultat  extends Activity
 		String [] array, array2;
 		TextView scoreNorm = popUp.getContentView().findViewById(R.id.scoreNorm);
 		String res;
+
 		for (int i = 0; i < output.size(); i++)
 		{
 			res = output.get(i);
+
 			if (i == 0)
-			{
 				scoreNorm.setText(res);
-			}
 			else if (i > 1)
 			{
 				array = res.split(":");
 				array2 = array[array.length-1].split("\\(");
 				TableRow tabLigne = new TableRow(this);
 				tabLigne.setBackgroundColor(Color.GRAY);
+
 				for (int j = 0; j < 3; j++)
 				{
 					TextView dataCol = new TextView(this);
@@ -283,14 +300,19 @@ public class Resultat  extends Activity
 					dataCol.setGravity(Gravity.CENTER_HORIZONTAL);
 					dataCol.setPadding(0, 5, 0, 5);
 					dataCol.setTextColor(Color.WHITE);
-					if(!array2[array2.length-1].substring(0,array2[array2.length-1].length()-1).equals("0)")) {
-						switch (j) {
+
+					if (!array2[array2.length-1].substring(0, array2[array2.length-1].length() - 1).equals("0)"))
+					{
+						switch (j)
+						{
 							case 0:
 								dataCol.setText(array2[0]);
 								break;
+
 							case 1:
 								dataCol.setText(array[0]);
 								break;
+
 							case 2:
 								dataCol.setText(array2[array2.length - 1].substring(0, array2[array2.length-1].length()-1));
 								break;
@@ -304,7 +326,6 @@ public class Resultat  extends Activity
 						dataCol.setText("");
 					}
 					tabLigne.addView(dataCol);
-
 				}
 				tab.addView(tabLigne);
 			}
