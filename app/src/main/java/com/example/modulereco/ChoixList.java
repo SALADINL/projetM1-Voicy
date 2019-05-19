e1fc6c5010501dc6fbf70ee562e803d1f1674938
Ahmet Agbektas <zamanyildiz@gmail.com>
2019-05-19 17:19:55 +0200

    Création des listes pour les logatomes (v2)

---
+++ app/src/main/java/com/example/modulereco/ChoixList.java
@@ -0,0 +1,82 @@
+package com.example.modulereco;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChoixList extends Activity
+{
+    private Button home;
+    private Button list1, list2, list3, list4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choix_list);
+
+        home = findViewById(R.id.home);
+
+        list1 = findViewById(R.id.list1);
+        list2 = findViewById(R.id.list2);
+        list3 = findViewById(R.id.list3);
+        list4 = findViewById(R.id.list4);
+
+        list1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChoixList.this, Reco.class);
+                intent.putExtra("numeroDeListe", 1);
+                startActivity(intent);
+            }
+        });
+
+        list2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChoixList.this, Reco.class);
+                intent.putExtra("numeroDeListe", 2);
+                startActivity(intent);
+            }
+        });
+
+        list3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChoixList.this, Reco.class);
+                intent.putExtra("numeroDeListe", 3);
+                startActivity(intent);
+            }
+        });
+
+        list4.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChoixList.this, Reco.class);
+                intent.putExtra("numeroDeListe", 4);
+                startActivity(intent);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChoixList.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+}
